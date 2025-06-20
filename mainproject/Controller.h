a103ae2355a4734cc52a86dfa02d77be61b23a17
Bảo <phungthienbao2009@gmail.com>
2025-06-20 18:43:40 +0700

    servo + slide

--- mainproject/Controller.h
+++ mainproject/Controller.h
@@ -102,4 +102,9 @@ void runn() {
     slidePower(3000, 0, 0, 3000);
     timer -= 1;
   }
+  int calib_servo = 0;
+  if (timer < 10 && calib_servo < 170) {
+    servoAngle(170 - calib_servo);
+    calib_servo += 10;
+  }
 }
